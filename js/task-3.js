50b822391aa6ec6f90d2f30300bf362bc44354a9
oleksandrkobzar90 <oleksandrkobzar@gmail.com>
2026-01-20 11:25:13 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+'use strict';
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  changeUsername(newUsername) {
+    this.username = newUsername;
+  },
+
+  updatePlayTime(newPlayTime) {
+    this.playTime = newPlayTime;
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
